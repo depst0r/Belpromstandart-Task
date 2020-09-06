eb0960754a41283f2f49ec00f4d67541d6cfddd9
depstor <depstor90@gmail.com>
2020-09-07 02:18:57 +0300

    animated

--- script/scriptJS.js
+++ script/scriptJS.js
@@ -1,18 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
     const cross = document.querySelectorAll('span'),
-        cercle = document.querySelectorAll('.aks-accordion-item-icon');
+        cercle = document.querySelectorAll('.aks-accordion-item-icon'),
+        row = document.querySelectorAll('.aks-accordion-item-row'),
+        content = document.querySelectorAll('.aks-accordion-item-content');
 
-
-
-    function animated(selector, style1, style2) {
-        selector.forEach(i => {
-            i.addEventListener('click', () => {
-                i.classList.toggle(style1);
-                i.classList.toggle(style2);
+    function changes(wrapper, arg, styleOne, styleTwo) {
+        wrapper.forEach((wrapper, i) => {
+            wrapper.addEventListener('click', () => {
+                arg[i].classList.toggle(styleOne);
+                arg[i].classList.toggle(styleTwo);
             })
         })
     }
 
-    animated(cercle, 'animated_cercle');
-    animated(cross, 'animated_cross', 'shadow');
+    changes(row, cercle, 'animated_cercle', '');
+    changes(row, cross, 'animated_cross', 'shadow');
+
 });
